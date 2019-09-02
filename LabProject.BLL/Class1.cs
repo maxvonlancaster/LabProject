05a3b3748f767ce703496f52155de1b28e6c9e42
maxvonlancaster <38358502+maxvonlancaster@users.noreply.github.com>
2019-09-02 18:50:45 +0300

    Add project files.

---
+++ LabProject.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LabProject.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ LabProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LabProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
